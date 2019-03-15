4dc79f3dbdf28b9d0151fc4014e609baaec4e235
Alexis Prabahaaran <alexisprabaharan@gmail.com>
2019-03-15 18:52:45 +0530

    march_15

---
+++ src/Gitorg/gitClass.java
@@ -0,0 +1,7 @@
+package Gitorg;
+
+public class gitClass {
+public static void main(String[] args) {
+	System.out.println("alex");
+}
+}
---
+++ src/org/sample/code.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class code {
+public static void main(String[] args) {
+	int i=100;
+			if (i==99){
+				System.out.println("True");
+			}
+				else
+					System.out.println("False");
+			}
+}
+
